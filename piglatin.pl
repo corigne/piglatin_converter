aff543d7e4bf8909b225152ac94a3222896d4497
Nathan Jodoin <corigne@gmail.com>
2022-11-21 09:25:24 -0600

    Cleaned up script.

--- piglatin.pl
+++ piglatin.pl
@@ -1,25 +1,18 @@
 #!/usr/bin/perl
-# Author: Nathan Jodoin
-# EUID njj0057
-# Lab 7 - Pig Latin Converter
-#modules
 use strict;
 use warnings;
 
-#script start
 our $s_userin;
-#do until userin eq quit
+
 do
 {
   # Prompt user.
   print("\nSentence to convert to pig latin? \n('quit' to exit): ");
-  # Get user input.
   chomp($s_userin = <STDIN>);
-  # Set up tokenized arr.
+
   our @tokens = split(/[ \t]/, $s_userin);
   our @originals = @tokens;
 
-  #iterate through 
   if($s_userin ne "quit")
   {
     print( "In piglatin: ");
@@ -30,9 +23,9 @@ do
     }
     
     # case preservation, check by char for each word in the original and recase
-    # if necessary, tested and working...
     for my $i(0 .. $#originals)
     {
+      
       my @token = split(//, $tokens[$i]);
       my @original = split(//, $originals[$i]);
       
@@ -47,11 +40,9 @@ do
         {
           $token[$j] = lc($token[$j]);
         }
-        
       }
-      
       $tokens[$i] = join('', @token);
-      
+
     }
     
     foreach my $word(@tokens)
@@ -61,7 +52,7 @@ do
     
     print ("\n");
   }
-}until($s_userin eq "quit");
-#Exit statement.
-print("Goodbye!\n\n");
 
+}until($s_userin eq "quit");
+
+print("Goodbye!\n\n");
